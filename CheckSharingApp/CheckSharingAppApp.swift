254549d9d249602d9eee085c76be3dc8077e19bb
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-05-13 17:04:59 +0300

    Initial Commit

---
+++ CheckSharingApp/CheckSharingAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSharingAppApp.swift
+//  CheckSharingApp
+//
+//  Created by Kristina Yaroshenko on 13.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct CheckSharingAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CheckSharingAppTests/CheckSharingAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CheckSharingAppTests.swift
+//  CheckSharingAppTests
+//
+//  Created by Kristina Yaroshenko on 13.05.2025.
+//
+
+import Testing
+@testable import CheckSharingApp
+
+struct CheckSharingAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
