65c14b596226a1c826ffa34e382a3d6699434da0
Kristina Yaroshenko <kris.jaroshenko09@gmail.com>
2025-05-13 18:27:55 +0300

    Initial commit with .gitignore

--- CheckSharingApp/ContentView.swift
+++ CheckSharingApp/ContentView.swift
@@ -8,17 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+    @State private var checkAmount = 0.0
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 20
+    
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages: [Int] = [0, 10, 15, 20, 25, 30]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal / peopleCount
     }
-}
+    
+    var totalCheck: Double {
+        let tipSelection = Double(tipPercentage)
+        
+        let tipValue = checkAmount / 100 * tipSelection
+        let grandTotal = checkAmount + tipValue
+        
+        return grandTotal
+    }
+    
+    var body: some View {
+        NavigationStack{
+            Form{
+                Section{
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of people", selection: $numberOfPeople){
+                        ForEach(2..<100){
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("How much tip do you want to leave?"){
+                    Picker("Tip percentage", selection: $tipPercentage){
+                        ForEach(0..<101){
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
+                }
+                
+                Section("Full check"){
+                    Text(totalCheck, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
 
-#Preview {
-    ContentView()
+                }
+                
+                Section ("Amount per person"){
+                    Text(totalPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle(Text("Check Splitter"))
+            .toolbar{
+                if amountIsFocused{
+                    Button("Done"){
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
 }
